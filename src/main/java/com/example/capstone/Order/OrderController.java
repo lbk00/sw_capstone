3fbfc38120a8c558a299967b2af9ed89f57017ee
lbk00 <lbk6661@gmail.com>
2024-10-14 11:30:31 +0900

    api 수정

--- src/main/java/com/example/capstone/Order/OrderController.java
+++ src/main/java/com/example/capstone/Order/OrderController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/orders")
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderServiceImpl ordersService;

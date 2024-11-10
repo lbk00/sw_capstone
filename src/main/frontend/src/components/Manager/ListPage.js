2d12af1e77ecfe5aaa72bb4287901fb5bd6ee9ab
lbk00 <lbk6661@gmail.com>
2024-11-10 10:46:31 +0900

    등록버튼

--- src/main/frontend/src/components/Manager/ListPage.js
+++ src/main/frontend/src/components/Manager/ListPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {useNavigate, useSearchParams} from "react-router-dom";
 import ListComponent from "./ListComponent";
-import {Button} from "@mui/material";
+import {Box, Button} from "@mui/material";
 import AddPage from "./AddPage";
 
 
@@ -23,14 +23,20 @@ import AddPage from "./AddPage";
 
     return (
     <div className="p-4 w-full bg-orange-200 ">
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
      <div className="text-3xl font-extrabold">
        공급업체 목록
      </div>
-     <ListComponent/>
-        <Button variant="contained" color="primary" onClick={handleOpen}>
+        <Button
+            variant="contained"
+            color="primary"
+            onClick={handleOpen}
+        >
             새로운 공급업체 등록
         </Button>
+        </Box>
         <AddPage open={openModal} onClose={handleClose} />
+     <ListComponent/>
 
 
     </div>

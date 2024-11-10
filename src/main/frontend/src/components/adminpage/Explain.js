e1a3f889fcef7794c9e7601a9a444892f37ffef9
lbk00 <lbk6661@gmail.com>
2024-11-10 16:55:18 +0900

    설명 화면

--- src/main/frontend/src/components/adminpage/Explain.js
+++ src/main/frontend/src/components/adminpage/Explain.js
@@ -63,10 +63,7 @@ export default function Explain() {
                         <Paper
                             elevation={hoveredCard === 0 ? 6 : 3}
                             sx={{
-                                height: 700,
-
-
-
+                                height: 650,
                                 p: 3
                             }}
                             onMouseEnter={() => handleMouseEnter(0)}
@@ -74,7 +71,7 @@ export default function Explain() {
                         >
                             <Stack direction="column" spacing={2}>
                                 <Stack direction="row" justifyContent="space-between">
-                                    <Typography variant="h5" component="div">주문서 관리</Typography>
+                                    <Typography variant="h5" component="div">납품 주문서 관리</Typography>
                                     <IconButton sx={{ p: 0 }}>
                                         <AssignmentIcon sx={{ fontSize: 30 }} />
                                     </IconButton>
@@ -91,27 +88,28 @@ export default function Explain() {
                                     title="주문서 관리"
                                 />
                                 <Typography variant="body1" component="div" whiteSpace="pre-line">
-                                    주문서 등록 : 새로운 주문서 정보 입력 및 등록{"\n"}
+                                    주문서 등록 : 새로운 주문서 정보 입력 및 등록{"\u00A0".repeat(30)}
                                     주문서 조회 : 모든 주문서 정보 조회{"\n"}
-                                    주문서 수정 : 선택한 주문서 정보 수정{"\n"}
-                                    주문서 삭제 : 선택한 주문서 정보 삭제
+                                    주문서 수정 및 삭제 : 선택한 주문서 정보 수정 및 삭제
+                                </Typography>
+                                <Typography sx={{ color: "gray" }} variant="body2" component="div" whiteSpace="pre-line">
+                                    참고사항 - 주문서는 수요일 밤 12:00 에 갱신됩니다.{"\n"}{"\u00A0".repeat(15)}
+                                    - 수요예측시 현재 상품의 재고를 입력값으로 사용합니다.
                                 </Typography>
                             </Stack>
                         </Paper>
                     </Grid>
                     {[
-                        { title: "공급업체 관리", icon: <BusinessIcon sx={{ fontSize: 30 }} />, description: "공급업체 등록 : 새로운 공급업체 정보 입력 및 등록\n공급업체 조회 : 모든 공급업체 정보 조회\n공급업체 수정 : 선택한 공급업체 정보 수정\n공급업체 삭제 : 선택한 공급업체 정보 삭제" },
-                        { title: "사용자 관리", icon: <PeopleIcon sx={{ fontSize: 30 }} />, description: "사용자 목록 : 현재 쇼핑몰의 관리자 정보 조회" },
+                        { title: "공급업체 관리", icon: <BusinessIcon sx={{ fontSize: 30 }} />, description: "공급업체 등록 : 새로운 공급업체 정보 입력 및 등록\n공급업체 조회 : 모든 공급업체 정보 조회\n공급업체 수정 및 삭제 : 선택한 공급업체 정보 수정 및 삭제"},
+                        { title: "사용자 관리", icon: <PeopleIcon sx={{ fontSize: 30 }} />, description: "사용자 조회 : 현재 쇼핑몰의 고객 및 관리자 정보 조회" },
                         { title: "재고 관리", icon: <InventoryIcon sx={{ fontSize: 30 }} />, description: "반품 품목 확인 : 반품 처리된 상품들의 리스트 조회\n납품 품목 확인 : 납품 처리된 상품들의 리스트 조회" },
-                        { title: "상품 관리", icon: <ShoppingCartIcon sx={{ fontSize: 30 }} />, description: "상품 등록 : 새로운 상품 정보 입력 및 등록\n상품 조회 : 모든 상품 정보 조회\n상품 수정 : 선택한 상품 정보 수정\n상품 삭제 : 선택한 상품 정보 삭제" },
-                        { title: "알림", icon: null, description: "주문서는 수요일 밤 12:00 에 갱신됩니다.\n수요예측시 현재 상품의 재고를 입력값으로 사용합니다." }
+                        { title: "상품 관리", icon: <ShoppingCartIcon sx={{ fontSize: 30 }} />, description: "상품 등록 : 새로운 상품 정보 입력 및 등록\n상품 조회 : 모든 상품 정보 조회\n상품 수정 및 삭제 : 선택한 상품 정보 수정 및 삭제" },
                     ].map((item, index) => (
                         <Grid item xs={12} md={6} key={index + 1}>
                             <Paper
                                 elevation={hoveredCard === index + 1 ? 6 : 3}
                                 sx={{
-                                    height: item.icon ? 300 : 150,
-
+                                    height: item.icon ? 250 : 150,
                                     p: 3
                                 }}
                                 onMouseEnter={() => handleMouseEnter(index + 1)}
